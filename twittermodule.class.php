93fde60b67253bc1e7176321253f507122474453
godmod <godmod@eqdkp-plus.eu>
2013-03-03 11:37:21 +0000

    - fixed search link in twitter module - fixed display of ITT

--- twittermodule.class.php
+++ twittermodule.class.php
@@ -284,7 +284,7 @@ class twittermodule extends gen_class {
 		$ret = preg_replace("#(^|[\n ])([\w]+?://[\w]+[^ \"\n\r\t< ]*)#", "\\1<a href=\"\\2\" target=\"_blank\">\\2</a>", $ret);
 		$ret = preg_replace("#(^|[\n ])((www|ftp)\.[^ \"\t\n\r< ]*)#", "\\1<a href=\"http://\\2\" target=\"_blank\">\\2</a>", $ret);
 		$ret = preg_replace("/@(\w+)/", "<a href=\"https://www.twitter.com/\\1\" target=\"_blank\">@\\1</a>", $ret);
-		$ret = preg_replace("/#(\w+)/", "<a href=\"https://search.twitter.com/search?q=%23\\1&amp;src=hash\" target=\"_blank\">#\\1</a>", $ret);
+		$ret = preg_replace("/#(\w+)/", "<a href=\"https://twitter.com/search?q=%23\\1&amp;src=hash\" target=\"_blank\">#\\1</a>", $ret);
 		return $ret;
 	}
 
