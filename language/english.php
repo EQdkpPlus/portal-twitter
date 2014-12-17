4e2ae1d4c723745b7b86d8a72cf82cd51b85e646
godmod <godmod@eqdkp-plus.eu>
2014-12-17 20:34:39 +0000

    Added languages

--- language/english.php
+++ language/english.php
@@ -1,6 +1,6 @@
 <?php
 /*	Project:	EQdkp-Plus
- *	Package:	Twitter Portal Module
+ *	Package:	EQdkp-Plus Language File
  *	Link:		http://eqdkp-plus.eu
  *
  *	Copyright (C) 2006-2015 EQdkp-Plus Developer Team
@@ -19,17 +19,54 @@
  *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-	$lang['twitter']				= 'Twitter';
-	$lang['twitter_name']			= 'Twitter';
-	$lang['twitter_desc']			= 'Follow a specific twitter account';
-	$lang['twitter_f_account']		= 'Twitter-Account';
-	$lang['twitter_f_maxitems']		= 'Max. Shown Tweeds (empty = no limit)';
-	$lang['twitter_f_cachetime']	= 'Caching-time in hours (default: 1 hour)';
-	$lang['twitter_f_hideuserreplys']	= 'Hide Replys to Users';
-	$lang['pm_twitter_follow']		= 'follow %s at twitter';
-	$lang['pm_twitter_period']		= array("second", "minute", "hour", "day", "week", "month", "year", "decade");
-	$lang['pm_twitter_periods']		= array("seconds", "minutes", "hours", "days", "weeks", "months", "years", "decades");
-	$lang['pm_twitter_tense']		= array("from now", "ago");
-	$lang['pm_twitter_format']		= '%1$s %2$s ';
+ 
+if (!defined('EQDKP_INC')) {
+	die('You cannot access this file directly.');
+}
+
+//Language: English	
+//Created by EQdkp Plus Translation Tool on  2014-12-17 21:28
+//File: portal/twitter/language/english.php
+//Source-Language: german
+
+$lang = array( 
+	"twitter" => 'Twitter',
+	"twitter_name" => 'Twitter',
+	"twitter_desc" => 'Follow a specific twitter account',
+	"twitter_f_account" => 'Twitter-Account',
+	"twitter_f_maxitems" => 'Max. Shown Tweeds (empty = no limit)',
+	"twitter_f_cachetime" => 'Caching-time in hours (default: 1 hour)',
+	"twitter_f_hideuserreplys" => 'Hide Replys to Users',
+	"pm_twitter_follow" => 'follow %s at twitter',
+	"pm_twitter_period" => array(
+	0 => 'second',
+	1 => 'minute',
+	2 => 'hour',
+	3 => 'day',
+	4 => 'week',
+	5 => 'month',
+	6 => 'year',
+	7 => 'decade',
+	),
+	"pm_twitter_periods" => array(
+	0 => 'seconds',
+	1 => 'minutes',
+	2 => 'hours',
+	3 => 'days',
+	4 => 'weeks',
+	5 => 'months',
+	6 => 'years',
+	7 => 'decades',
+	),
+	"pm_twitter_tense" => array(
+	0 => 'from now',
+	1 => 'ago',
+	),
+	"pm_twitter_format" => '%1$s %2$s ',
+	"pm_twitter_answer" => 'Reply',
+	"pm_twitter_retweet" => 'Retweet',
+	"pm_twitter_favorit" => 'Favorite',
+	
+);
 
 ?>
